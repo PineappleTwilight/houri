1f1db6b03e79b161329806fc4e24c47277c4004a
amnesia <anoN@example.com>
2026-08-17 16:47:29 -0400

    chore: bump version to 1.17.5 (110)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
     defaultConfig {
         applicationId = "app.houri"
 
-        versionCode = 109
-        versionName = "1.17.4"
+        versionCode = 110
+        versionName = "1.17.5"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
