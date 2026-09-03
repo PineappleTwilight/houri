918c2814d44442c9a3880b22f2271144acbd39b5
amnesia <anoN@example.com>
2026-09-02 20:31:16 -0400

    Update gitignore + bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "app.houri"
 
         // These need to be updated for every new release that should be automatically pushed
-        versionCode = 150
-        versionName = "1.21.5"
+        versionCode = 151
+        versionName = "1.21.6"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
