6d3cb8028f482c53113814f11d775f1406757dce
amnesia <anoN@example.com>
2026-09-04 13:04:28 -0400

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "app.houri"
 
         // These need to be updated for every new release that should be automatically pushed
-        versionCode = 153
-        versionName = "1.21.8"
+        versionCode = 154
+        versionName = "1.21.9"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
