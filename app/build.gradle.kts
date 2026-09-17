bb7472094be1107abfeed9bfc779aa147eb6bc3c
amnesia <anoN@example.com>
2026-09-17 14:51:46 -0400

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "app.houri"
 
         // These need to be updated for every new release that should be automatically pushed
-        versionCode = 170
-        versionName = "1.22.5"
+        versionCode = 171
+        versionName = "1.22.6"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
