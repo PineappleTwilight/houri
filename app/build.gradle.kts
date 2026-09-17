5ec34ebc2f0ad0e072c1a9741fcc675ddba95e81
amnesia <anoN@example.com>
2026-09-17 01:50:52 -0400

    Fixed some webgpu bugs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "app.houri"
 
         // These need to be updated for every new release that should be automatically pushed
-        versionCode = 169
-        versionName = "1.22.4"
+        versionCode = 170
+        versionName = "1.22.5"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
