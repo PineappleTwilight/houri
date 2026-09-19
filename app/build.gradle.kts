cd840298af7d876c44738e3666e32051c626c6bf
PineappleTwilight <171878891+PineappleTwilight@users.noreply.github.com>
2026-09-19 16:45:00 -0400

    Bugfix for sequels/prequels and webgpu icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "app.houri"
 
         // These need to be updated for every new release that should be automatically pushed
-        versionCode = 176
-        versionName = "1.22.11"
+        versionCode = 177
+        versionName = "1.22.12"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
