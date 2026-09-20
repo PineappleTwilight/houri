89dc2854e64d7c9281a558f44acea10cd8d3e60a
PineappleTwilight <171878891+PineappleTwilight@users.noreply.github.com>
2026-09-19 20:39:37 -0400

    Bugfix for sequels/prequels again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "app.houri"
 
         // These need to be updated for every new release that should be automatically pushed
-        versionCode = 177
-        versionName = "1.22.12"
+        versionCode = 178
+        versionName = "1.22.13"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
